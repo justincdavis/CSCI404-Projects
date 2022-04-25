2c1f94de1be648371090ad99145843c429b1562d
justincdavis <jcdavis@mssm.org>
2022-04-25 16:17:44 -0600

    fixed

--- Project1/find_route.py
+++ Project1/find_route.py
@@ -20,15 +20,26 @@ def read_file(filename):
     return graph
 
 
-def find_path(graph, origin_city, dest_city):
+def find_path(graph, origin_city, dest_city, max_searches=None):
+    if max_searches is None:
+        max_searches = len(graph) ** 3
+    total_searches = 0
+
     queue = PriorityQueue()
     queue.put((0, (origin_city, origin_city)))
 
-    path = {origin_city: dest_city}
+    path = {origin_city: (origin_city, 0)}
 
     while not queue.empty():
         distance, (city1, city2) = queue.get()
-        path[city1] = city2
+
+        if total_searches > max_searches:
+            break
+
+        if city1 in path and distance < path[city1][1]:
+            path[city1] = (city2, distance)
+        elif city1 not in path:
+            path[city1] = (city2, distance)
 
         if city1 == dest_city:
             return path, distance
@@ -37,16 +48,43 @@ def find_path(graph, origin_city, dest_city):
                 new_distance += distance
                 queue.put((new_distance, (city, city1)))
 
+        total_searches += 1
+
+    return None, "inf"
+
 
 def get_path(path, origin_city, dest_city):
+    if path is None:
+        return None
+
     final_path = [dest_city]  # create a list too represent the final path
-    next_city = path[dest_city]
+    next_city = path[dest_city][0]
 
     while next_city != origin_city:
         final_path.append(next_city)
-        next_city = path[next_city]
+        next_city = path[next_city][0]
 
-    return final_path.reverse()
+    if len(final_path) > 1:
+        final_path.append(origin_city)
+    final_path.reverse()
+    return final_path
+
+
+def print_output(graph, path, distance, origin_city, dest_city):
+    if path is None:
+        print("distance: infinity")
+        print("route")
+        print("none")
+    else:
+        print(f"distance: {distance}")
+        print("route")
+        for idx, city in enumerate(path):
+            if idx < len(path) - 2:
+                next_city = path[idx + 1]
+                print(f"{city} to {next_city}, {graph[city][next_city]}km")
+            else:
+                dist = 0 if city == dest_city else graph[city][dest_city]
+                print(f"{city} to {dest_city}, {dist}km")
 
 
 if __name__ == '__main__':
@@ -58,3 +96,5 @@ if __name__ == '__main__':
     route, final_dist = find_path(world, origin, dest)
 
     list_route = get_path(route, origin, dest)
+
+    print_output(world, list_route, final_dist, origin, dest)
